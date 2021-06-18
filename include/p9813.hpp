473af7a8be9801bb7895c7b39f956ebe610da1c9
Ben Mens <ben.mens@gmail.com>
2021-06-18 21:27:57 +0200

    initial commit

---
+++ include/p9813.hpp
@@ -0,0 +1,26 @@
+#ifndef _P9813_H
+#define _P9813_H
+
+#include <stdio.h>
+#include "driver/spi_master.h"
+
+class P9813 {
+  private:
+    spi_device_handle_t spi;
+    uint8_t *tx_buffer;
+    int numModules;
+
+  public:
+    P9813(
+      int numModules,
+      spi_host_device_t spiHost, 
+      int dmaChan, 
+      int pinMosi, 
+      int pinSclk, 
+      int pinCS);
+
+    void writeColor(int index, uint8_t red, uint8_t green, uint8_t blue);
+};
+
+
+#endif
---
+++ p9813.cpp
@@ -0,0 +1,76 @@
+#include <p9813.hpp>
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define TAG "P9813"
+
+P9813::P9813(
+    int numModules,
+    spi_host_device_t spiHost, 
+    int dmaChan, 
+    int pinMosi, 
+    int pinSclk, 
+    int pinCS)
+  : numModules(numModules) {
+
+  spi_bus_config_t buscfg = {};
+  buscfg.mosi_io_num = pinMosi;
+  buscfg.miso_io_num = -1;
+  buscfg.sclk_io_num = pinSclk;
+  buscfg.quadwp_io_num = -1;
+  buscfg.quadhd_io_num = -1;
+  buscfg.max_transfer_sz = 3 * 4;
+
+  spi_device_interface_config_t devcfg = {};
+  devcfg.command_bits = 0;
+  devcfg.address_bits = 0;
+  devcfg.clock_speed_hz = 10 * 1000 * 1000;
+  devcfg.mode = 0;
+  devcfg.spics_io_num = pinCS; 
+  devcfg.queue_size = 1;                      
+
+  ESP_ERROR_CHECK(spi_bus_initialize(spiHost, &buscfg, dmaChan));
+
+  ESP_ERROR_CHECK(spi_bus_add_device(spiHost, &devcfg, &spi));
+
+  tx_buffer = (uint8_t *)heap_caps_malloc((numModules + 2) * 4, MALLOC_CAP_DMA);
+
+  tx_buffer[ 0] = 0x00;
+  tx_buffer[ 1] = 0x00;
+  tx_buffer[ 2] = 0x00;
+  tx_buffer[ 3] = 0x00;
+
+  tx_buffer[numModules * 4 + 0] = 0x00;
+  tx_buffer[numModules * 4 + 1] = 0x00;
+  tx_buffer[numModules * 4 + 2] = 0x00;
+  tx_buffer[numModules * 4 + 3] = 0x00;
+}
+
+
+void P9813::writeColor(int index, uint8_t red, uint8_t green, uint8_t blue) {
+  static spi_transaction_t trans = {};
+
+  trans.length = (numModules + 2)  * 4 * 8;
+  trans.tx_buffer = tx_buffer;
+  trans.rx_buffer = nullptr;
+
+  int offset = (index + 1) * 4;
+
+    // Start by sending a byte with the format "1 1 /B7 /B6 /G7 /G6 /R7 /R6"
+  tx_buffer[offset] = (1<<6) | (1<<7);
+  if ((blue & 0x80) == 0)  tx_buffer[offset] |= (1<<5);
+  if ((blue & 0x40) == 0)  tx_buffer[offset] |= (1<<4);
+  if ((green & 0x80) == 0) tx_buffer[offset] |= (1<<3);
+  if ((green & 0x40) == 0) tx_buffer[offset] |= (1<<2);
+  if ((red & 0x80) == 0)   tx_buffer[offset] |= (1<<1);
+  if ((red & 0x40) == 0)   tx_buffer[offset] |= (1<<0);
+
+  tx_buffer[offset + 1] = blue;
+  tx_buffer[offset + 2] = green;
+  tx_buffer[offset + 3] = red;
+
+  ESP_ERROR_CHECK(spi_device_polling_start(spi, &trans, portMAX_DELAY));
+
+  ESP_ERROR_CHECK(spi_device_polling_end(spi, portMAX_DELAY));
+}
